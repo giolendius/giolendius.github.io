6e4d862ff2865cf01b57e1f1efec580d8b9bd3c6
Gioele Gallo <155963300+giolendius@users.noreply.github.com>
2024-12-15 10:02:53 +0100

    Update constants.js

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,5 @@
 let countdown_timeout = 1000;
-let env = "dev";
+let env = "prod";
 let keys = {
     "dev": "AIzaSyCj5yLYFOBinCuQ1WgYYSWL6uOdjfEGFeQ",
     "prod": "AIzaSyCH7XOLG0wx22AFrZw6N3_JFjwqPsMt41c"
