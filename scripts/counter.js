15c8833bbb4ed50e23e3eb9c8a9b295def24c38e
giolendius <155963300+giolendius@users.noreply.github.com>
2024-12-26 10:32:14 +0100

    feat(style): games counter
    added a button to "calculate" the games number

--- scripts/counter.js
+++ scripts/counter.js
@@ -1,4 +1,47 @@
 // Automatic Slideshow - change image every 3 seconds
+
+function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+function calcola_numero_giochi(json)
+{   let tot = json["values"].length-1;
+    console.log(tot)
+    let bottone = document.getElementById("calcola");
+    bottone.addEventListener("click", function () {
+        animateValue(obj, 0, tot, 1500) });
+        }
+
+const obj = document.getElementById("tot_giochi");
+let chiave = keys[env]
+let schl = {
+    "dev": "1WgYYSWL6uOdjfEGFeQ",
+    "prod": "Zw6N3_JFjwqPsMt41c",
+    "prod2": "g2eGJy8CEaahauw"
+}
+let sheetLink = "https://sheets.googleapis.com/v4/spreadsheets/" +
+    "1RnaUmV5fSHc3oyIf62DhY3m21oLaS6xh2ss3KcMzKn8" +
+    // "1YuvMg055gT-pA0brzaKnK9PJqH8Z0bGdPez79sdWR8c" +
+    "/values/" +
+    "Database" +
+    "/?key=AIzaSyC" + chiave + schl[env];
+
+fetch(sheetLink).then(response => response.json())
+    .catch(error => console.error('Error fatching Google sheet:', error))
+    .then(json => calcola_numero_giochi(json))
+
+
+
+
 var myIndex = 0;
 carousel();
 
