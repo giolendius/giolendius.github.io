e58aa7923f0b6080577ba26fdbc8e0c3b153d0f7
giolendius <155963300+giolendius@users.noreply.github.com>
2025-12-12 20:38:06 +0100

    feat(ts): test 12

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ function helloWorld() {
   const element = document.getElementById('hello-world')
 
   if (element) {
-    element.textContent ='Hello, World!'
+    element.textContent = 'Hello, World!'+process.env.SUPER_SECRET
     }
 
 }
